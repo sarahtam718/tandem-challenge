06e409258f3dfdff13521effe98e3484f2a595b6
Sarah Tam <sarahtam718@gmail.com>
2019-12-12 06:22:03 -0600

    set up basic file structure & first component

---
+++ watering-scheduler/src/components/PlantMath.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import plantData from './plantData.json';
+
+export default class PlantMath extends Component {
+  render() {
+    return (
+      <div>
+        <h1>First Component!</h1>
+      </div>
+    );
+  }
+}
