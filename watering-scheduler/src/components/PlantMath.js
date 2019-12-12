49cfbcdb62d69ca8d444da95d8cf9c3b15d8d0b4
Sarah Tam <sarahtam718@gmail.com>
2019-12-12 07:27:49 -0600

    created new array of plant objects without string containing days

--- watering-scheduler/src/components/PlantMath.js
+++ watering-scheduler/src/components/PlantMath.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import plantData from './plantData.json';
 
+// Empty global array to fill with dates to water each plant
+let wateringDates = [];
+
 export default class PlantMath extends Component {
   // How many times do we need to water the plants in the next 12 weeks?
   // We need total days of watering divided by frequency of watering (Ex. Need to see dentist every 6 months in a year, so 12 total months in a year / 6 months frequency = 2x per year)
@@ -8,13 +11,15 @@ export default class PlantMath extends Component {
   // can find frequency by splitting water_after value
 
   wateringFrequency = () => {
-    console.log(plantData);
+    // console.log(plantData);
     for (let i = 0; i < plantData.length; i++) {
       let eachPlant = plantData[i].name;
-      let waterDays = plantData[i].water_after.split(' ')[0];
-
-      console.log(eachPlant, waterDays);
+      let waterFreq = plantData[i].water_after.split(' ')[0];
+      //console.log(eachPlant, waterFreq);
+      //wateringDates.push(eachPlant, waterFreq);
+      wateringDates.push({ eachPlant: eachPlant, waterFreq: waterFreq });
     }
+    console.log(wateringDates);
   };
 
   render() {
