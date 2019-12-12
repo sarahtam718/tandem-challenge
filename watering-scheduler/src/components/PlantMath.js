46c2a565c7192ca307b9734a1bf6806331f99b09
Sarah Tam <sarahtam718@gmail.com>
2019-12-12 07:00:17 -0600

    drilled into data for frequency of watering

--- watering-scheduler/src/components/PlantMath.js
+++ watering-scheduler/src/components/PlantMath.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-// import plantData from './plantData.json';
+import plantData from './plantData.json';
 
 export default class PlantMath extends Component {
+  // How many times do we need to water the plants in the next 12 weeks?
+  // We need total days of watering divided by frequency of watering (Ex. Need to see dentist every 6 months in a year, so 12 total months in a year / 6 months frequency = 2x per year)
+  // react-moment can provide total number of days, but I'll use a placeholder for now
+  // can find frequency by splitting water_after value
+
+  wateringFrequency = () => {
+    console.log(plantData);
+    for (let i = 0; i < plantData.length; i++) {
+      let eachPlant = plantData[i].name;
+      let waterDays = plantData[i].water_after.split(' ')[0];
+
+      console.log(eachPlant, waterDays);
+    }
+  };
+
   render() {
     return (
       <div>
-        <h1>First Component!</h1>
+        <h1 run={this.wateringFrequency()}>Watering Scheduler</h1>
       </div>
     );
   }
