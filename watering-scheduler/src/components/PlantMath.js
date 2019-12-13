6fe2c3fec1d11543d1a5a0d710712eee41b31a04
Sarah Tam <sarahtam718@gmail.com>
2019-12-12 19:12:46 -0600

    Progress toward drilling down into the data to pull out what is needed

--- watering-scheduler/src/components/PlantMath.js
+++ watering-scheduler/src/components/PlantMath.js
@@ -4,33 +4,67 @@ import Moment from 'react-moment';
 
 // Empty global array to fill with dates to water each plant
 let wateringDates = [];
-let timesToWater = [];
 const startDate = '2019-12-16';
 
 export default class PlantMath extends Component {
-  // How many times do we need to water the plants in the next 12 weeks?
-  // We need total days of watering divided by frequency of watering (Ex. Need to see dentist every 6 months in a year, so 12 total months in a year / 6 months frequency = 2x per year)
-  // react-moment can provide total number of days
-  // can find frequency by splitting water_after value
-
   // for each plant, how many times will we have to water during the 12 weeks?
+  // we need total days of watering divided by frequency of watering (Ex. Need to see dentist every 6 months in a year, so 12 total months in a year / 6 months frequency = 2x per year)
   wateringFrequency = () => {
     // console.log(plantData);
     const numOfDays = 12 * 7;
     // console.log(numOfDays);
-    for (let i = 0; i < plantData.length; i++) {
+    for (let i = 0; i < 1; i++) {
       let eachPlant = plantData[i].name;
       let waterFreq = plantData[i].water_after.split(' ')[0];
       //console.log(eachPlant, waterFreq);
       let numOfWaterings = numOfDays / Number(waterFreq);
+      console.log(numOfWaterings);
+      for (let j = 0; j < numOfWaterings; j++) {
+        console.log(
+          eachPlant,
+          'says water me! My frequency is',
+          waterFreq,
+          'days'
+        );
+      }
+      // --------------------------------------
+      //   for (let i = 0; i < plantData.length; i++) {
+      //     let eachPlant = plantData[i].name;
+      //     let waterFreq = plantData[i].water_after.split(' ')[0];
+      //     //console.log(eachPlant, waterFreq);
+      //     let numOfWaterings = numOfDays / Number(waterFreq);
+      //     console.log(numOfWaterings);
+      //     for (let j = 0; j < numOfWaterings; j++) {
+      //       console.log(eachPlant, 'water me!');
+      //     }
+      // -------------------------------
+      //   console.log(numOfWaterings);
+      //   for (let j = 0; j < numOfWaterings; j++) {
+      //     console.log(eachPlant, 'water me!');
+      //   }
+
       // now we can push the name of the plant and the # of waterings
-      wateringDates.push({
-        eachPlant: eachPlant,
-        numOfWaterings: numOfWaterings
-      });
+      //   wateringDates.push({
+      //     eachPlant: eachPlant,
+      //     numOfWaterings: numOfWaterings
+      //   });
+
+      //   let something = wateringDates[i].numOfWaterings;
     }
-    console.log(wateringDates);
+
+    // for (let k = 0; k < wateringDates.length; k++) {
+    //   console.log(wateringDates[k].eachPlant, something);
+    // }
+    // console.log(wateringDates);
   };
+
+  // What are the specific dates for these waterings?
+  //   waterMeOn = () => {
+  //     for (let i = 0; i < wateringDates.length; i++) {
+  //       console.log(wateringDates[i].numOfWaterings);
+  //       console.log(plantData);
+  //     }
+  //   };
 
   render() {
     return (
