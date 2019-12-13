c3b5ba2761e49905d8608194201b6d5615ab0445
Sarah Tam <sarahtam718@gmail.com>
2019-12-12 18:40:38 -0600

    combined two functions into finding number of waterings

--- watering-scheduler/src/components/PlantMath.js
+++ watering-scheduler/src/components/PlantMath.js
@@ -13,41 +13,29 @@ export default class PlantMath extends Component {
   // react-moment can provide total number of days
   // can find frequency by splitting water_after value
 
+  // for each plant, how many times will we have to water during the 12 weeks?
   wateringFrequency = () => {
     // console.log(plantData);
+    const numOfDays = 12 * 7;
+    // console.log(numOfDays);
     for (let i = 0; i < plantData.length; i++) {
       let eachPlant = plantData[i].name;
       let waterFreq = plantData[i].water_after.split(' ')[0];
       //console.log(eachPlant, waterFreq);
-      let numFreq = Number(waterFreq);
-      // now we can push the name of the plant and the frequency in number format
-      wateringDates.push({ eachPlant: eachPlant, waterFreq: numFreq });
-    }
-    // console.log(wateringDates);
-  };
-
-  // for each plant, how many times will we have to water during the 12 weeks?
-  howManyWaterings = () => {
-    // total # of days in 12 weeks
-    let numOfDays = 12 * 7;
-    //console.log(numOfDays);
-    // for each plant, divide # of days / water_after frequency to find # of waterings
-    for (let i = 0; i < wateringDates.length; i++) {
-      const numOfWaterings = numOfDays / wateringDates[i].waterFreq;
-      timesToWater.push({
-        plant: wateringDates[i].eachPlant,
+      let numOfWaterings = numOfDays / Number(waterFreq);
+      // now we can push the name of the plant and the # of waterings
+      wateringDates.push({
+        eachPlant: eachPlant,
         numOfWaterings: numOfWaterings
       });
     }
-    console.log(timesToWater);
+    console.log(wateringDates);
   };
 
   render() {
     return (
       <div>
-        <h3 first={this.wateringFrequency()} run={this.howManyWaterings()}>
-          Your Last Turn to Water is
-        </h3>
+        <h3 first={this.wateringFrequency()}>Your Last Turn to Water is</h3>
         {/* end date is March 9th */}
         <Moment add={{ weeks: 12 }}>{startDate}</Moment>
       </div>
